0cb0bdd9e3d115eadb6fa9e83c6759121c328315
Tai <hotutai2002@gmail.com>
2025-03-17 13:33:25 +0700

    Update register and login

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,81 @@
+import { Eye, EyeOff } from 'lucide-react'
+import React from 'react'
+import Footer from '../components/Footer'
+import Register from './Register'
+import { Link } from 'react-router-dom'
+const Login = () => {
+  return (
+    <><div className='w-full bia-bg h-screen z-50'>
+    <div className='h-[92px]'>
+    <header className='flex justify-between items-center px-[300px] h-full' >
+         <div className=''><img className='h-[40px] w-[142px] bg-transparent' src="logo-removebg-preview.png" alt="" /></div>
+        
+    </header>
+    </div>
+
+    <div className=" flex  justify-center ">
+    <div className="bg-black  text-white p-8 rounded-lg shadow-lg w-full max-w-sm">
+      <h2 className="text-3xl font-semibold text-center  mb-6">Đăng Nhập</h2>
+      
+      <form >
+        {/* Email Input */}
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-sm font-medium text-gray-600">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+           
+            className="mt-2 p-3 w-full border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+            placeholder="Nhập email của bạn"
+            required
+          />
+        </div>
+
+        {/* Password Input */}
+        <div className="mb-6 relative">
+          <label htmlFor="password" className="block text-sm font-medium text-gray-600">Mật khẩu</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+          
+            className="mt-2 p-3 w-full border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+            placeholder="Nhập mật khẩu của bạn"
+            required
+          />
+          <Eye className='size-6 absolute top-[40px] right-[12px] text-white'/>
+          {/* <EyeOff className='size-6 absolute top-[40px] right-[12px] text-white'/> */}
+        </div>
+
+        {/* Submit Button */}
+        <button
+          type="submit"
+          className="w-full bg-red-500 text-white p-3 rounded-lg hover:opacity-80 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        >
+          Đăng Nhập
+        </button>
+      </form>
+
+      {/* Forgot Password Link */}
+      <div className="mt-4 flex justify-between">
+         <div>
+            <input type="checkbox" className=''/>
+            <span className='ml-2'>Nhớ mật khẩu</span>
+         </div>
+        <a href="#" className="text-sm text-red-500 hover:underline">Quên mật khẩu?</a>
+      </div>
+      <div className='mt-4'>
+        <span>Bạn chưa có tài khoản?</span> <Link to={"/register"} className='hover:underline'>Đăng ký.</Link>
+      </div>
+    </div>
+  </div>
+  </div>
+    <Footer/>
+  </>
+    
+  
+  )
+}
+
+export default Login
