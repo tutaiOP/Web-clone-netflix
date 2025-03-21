fbf7a2cb3523c20de622753249c8a3857e059c0b
Tai <hotutai2002@gmail.com>
2025-03-21 09:19:31 +0700

    test

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import { Link } from 'react-router-dom'
-import { Bell, Search, UserRound } from 'lucide-react'
+import { Bell, Search, User, CircleHelp, LogOut } from 'lucide-react'
 const Header = () => {
+  const [isHoveredAvatar, setIsHoveredAvatar] = useState(false)
+  const [isHoveredBell, setIsHoveredBell] = useState(false)
+
   return (
     <div className='relative'>
        <header className="h-[60px] flex justify-between px-13 items-center bg-transparent absolute top-0 left-0 right-0" >
@@ -15,21 +18,27 @@ const Header = () => {
    
     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
       <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border   rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-        <li>
-          <a href="#" className="block py-2 px-3 text-white bg-black rounded-sm md:bg-transparent md:text-white md:p-0 dark:text-white md:dark:text-black" aria-current="page">Trang chủ</a>
-        </li>
-        <li>
-          <a href="#" className="block py-2 px-3 text-white rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:opacity-80 md:p-0 dark:text-white md:dark:hover:text-black  dark:hover:text-white md:dark:hover:bg-transparent">Series</a>
-        </li>
-        <li>
-          <a href="#" className="block py-2 px-3 text-white rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:opacity-80 md:p-0 dark:text-white md:dark:hover:opacity-80  dark:hover:text-white md:dark:hover:bg-transparent">Phim</a>
-        </li>
-        <li>
-          <a href="#" className="block py-2 px-3 text-white rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:opacity-80 md:p-0 dark:text-white md:dark:hover:opacity-80  dark:hover:text-white md:dark:hover:bg-transparent">Mới & Phổ biến</a>
-        </li>
-        <li>
-          <a href="#" className="block py-2 px-3 text-white rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:opacity-80 md:p-0 dark:text-white md:dark:hover:opacity-80  dark:hover:text-white md:dark:hover:bg-transparent">Danh sách của tôi</a>
-        </li>
+       
+          <Link to={"/"} className=''>
+          <li>
+          <span className="block py-2 px-3 text-white bg-black rounded-sm md:bg-transparent md:text-white md:p-0 dark:text-white md:dark:text-black" aria-current="page">Trang chủ</span>
+          </li>
+          </Link>
+          <Link to={"/genre"} className=''>
+          <li>
+          <span className="block py-2 px-3 text-white bg-black rounded-sm md:bg-transparent md:text-white md:p-0 dark:text-white md:dark:text-black" aria-current="page">Series</span>
+          </li>
+          </Link>
+          <Link to={"/latest"} className=''>
+          <li>
+          <span className="block py-2 px-3 text-white bg-black rounded-sm md:bg-transparent md:text-white md:p-0 dark:text-white md:dark:text-black" aria-current="page">Mới & phổ biến</span>
+          </li>
+          </Link>
+        <Link to={"/my-list"} className=''>
+          <li>
+          <span className="block py-2 px-3 text-white bg-black rounded-sm md:bg-transparent md:text-white md:p-0 dark:text-white md:dark:text-black" aria-current="page">Danh sách của tôi</span>
+          </li>
+          </Link>
       </ul>
     </div>
   </div>
@@ -40,15 +49,123 @@ const Header = () => {
             <div className='flex justify-center items-center relative'>
            
           <input type="text" id="search-navbar" 
-          className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..."/>
-             <Search className='absolute top-[5px] left-[5px] text-gray-400'/>
+          className="block w-full p-2 ps-10 text-sm text-white border border-gray-300 rounded-lg bg-transparent focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..."/>
+             <Search className='absolute top-[5px] left-[5px] text-white'/>
             </div>
-            <Bell className='mx-5'/>
-            <UserRound />
+
+            <div className='relative'
+            onMouseEnter={() => setIsHoveredBell(true)}
+            onMouseLeave={() => setIsHoveredBell(false)}
+            >
+          
+             <Bell className='mx-5  cursor-pointer'/>
+              {isHoveredBell && (
+                <>
+                  <div className='triangle-up absolute w-0 h-0 top-6 cursor-pointer right-[27px]'></div>
+                  <div className='absolute top-[34px] right-[15px] w-90 h-100 bg-black rounded-lg  duration-150 overflow-y-scroll '>
+                 <div className='border-b-2 border-gray-800 mb-2'>
+                 <div className='flex mb-2 mx-2 relative my-3'>
+                    <div className='w-50 h-20 mr-2 '>
+                      <img src="phi-vu-trieu-do.webp" className='w-full h-full object-cover rounded-lg' alt="" />
+                    </div>
+                    <div>
+                         <h2 className='text-[14px] '>Ra mắt vào Ngày 22 tháng 3 Xem giới thiệu ngay hôm nay.</h2>
+                         <span className='text-[12px] text-gray-500'>Hôm nay</span>
+                    </div>
+
+                  </div>
+                 </div>
+                 <div className='border-b-2 border-gray-800 mb-2'>
+                 <div className='flex mb-2 mx-2 relative my-3'>
+                    <div className='w-50 h-20 mr-2 '>
+                      <img src="one-piece.jpg" className='w-full h-full object-cover rounded-lg' alt="" />
+                    </div>
+                    <div>
+                         <h2 className='text-[14px] '>Ra mắt vào Ngày 22 tháng 3 Xem giới thiệu ngay hôm nay.</h2>
+                         <span className='text-[12px] text-gray-500'>Hôm nay</span>
+                    </div>
+
+                  </div>
+                 </div>
+                 <div className='border-b-2 border-gray-800 mb-2'>
+                 <div className='flex mb-2 mx-2 relative my-3'>
+                    <div className='w-50 h-20 mr-2 '>
+                      <img src="one-piece.jpg" className='w-full h-full object-cover rounded-lg' alt="" />
+                    </div>
+                    <div>
+                         <h2 className='text-[14px] '>Ra mắt vào Ngày 22 tháng 3 Xem giới thiệu ngay hôm nay.</h2>
+                         <span className='text-[12px] text-gray-500'>Hôm nay</span>
+                    </div>
+
+                  </div>
+                 </div>
+                 <div className='border-b-2 border-gray-800 mb-2'>
+                 <div className='flex mb-2 mx-2 relative my-3'>
+                    <div className='w-50 h-20 mr-2 '>
+                      <img src="one-piece.jpg" className='w-full h-full object-cover rounded-lg' alt="" />
+                    </div>
+                    <div>
+                         <h2 className='text-[14px] '>Ra mắt vào Ngày 22 tháng 3 Xem giới thiệu ngay hôm nay.</h2>
+                         <span className='text-[12px] text-gray-500'>Hôm nay</span>
+                    </div>
+
+                  </div>
+                 </div>
+                 <div className='border-b-2 border-gray-800 mb-2'>
+                 <div className='flex mb-2 mx-2 relative my-3'>
+                    <div className='w-50 h-20 mr-2 '>
+                      <img src="one-piece.jpg" className='w-full h-full object-cover rounded-lg' alt="" />
+                    </div>
+                    <div>
+                         <h2 className='text-[14px] '>Ra mắt vào Ngày 22 tháng 3 Xem giới thiệu ngay hôm nay.</h2>
+                         <span className='text-[12px] text-gray-500'>Hôm nay</span>
+                    </div>
+
+                  </div>
+                 </div>
+           </div>
+                  </>
+               
+                )}
+            
+            </div>
+          
+
+
+           <div
+            className='w-13 h-13 relative'
+            onMouseEnter={() => setIsHoveredAvatar(true)}
+            onMouseLeave={() => setIsHoveredAvatar(false)}
+            >
+          
+            <img src="/avatar-1.png" alt="Avatar" className='object-contain cursor-pointer' />
+            {isHoveredAvatar && (
+              <>
+                <div className='triangle-up absolute w-0 h-0 bottom-[-2px] right-[19px]'></div>
+               <div className='absolute top-[54px] right-[10px] w-55 m-h-24 bg-black rounded-lg p-4 duration-150 '>
+               <div className='flex flex-col '>
+                  <div className='flex mb-2 '>
+                     <User/>
+                      <a href="" className='ml-2 hover:underline'> Tài khoản</a>
+                  </div>
+                  <div className='flex mb-2 '>
+                  <CircleHelp/>
+                      <a href="" className='ml-2 hover:underline'> Trung tâm trợ giúp</a>
+                  </div>
+                  <div className='flex  '>
+                  <LogOut/>
+                      <a href="" className='ml-2 hover:underline'> Đăng xuất</a>
+                  </div>
+               </div>
+          </div>
+              </>
+              
+            )}
+           </div>
             </div>
        </header>
-        <div>
-          <img src="image.png" alt="" />
+        <div className='w-full'>
+          <img className='w-full h-[800px]' src="image.png" alt="" />
         </div>
     </div>
   )
--- frontend/src/components/MovieSlider.jsx
+++ frontend/src/components/MovieSlider.jsx
@@ -1,138 +1,175 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { FreeMode, Pagination } from "swiper/modules";
+
+import { RxArrowTopRight } from "react-icons/rx";
+
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const MovieSlider = () => {
-  return (
-    <div className='text-white bg-black px-5 md:px-20 relative'>
-      <h2 className='mb-4 text-2xl font-bold'>Top 10 bộ phim truyền hình</h2>
-
-      <div className='flex space-x-4 overflow-x-scroll scrollbar-hide'  >
-              <Link to={`/watch/1`} className=' max-w-[243px] relative group' >
-                  <div className='rounded-lg overflow-hidden'>
-                      <img
-                          src="one-piece.jpg"
-                          alt='Movie image'
-                          className='transition-transform duration-300 ease-in-out group-hover:scale-125 w-[243px] h-[136px]'
-                      />
-                      <div className='absolute top-0 left-2 text-red-600 font-bold'>N</div>
-                  </div>
-                  <p className='mt-2 text-left text-lg truncate'>One Piece</p>
-              </Link>
-              <Link to={`/watch/1`} className=' max-w-[243px] relative group' >
-                  <div className='rounded-lg overflow-hidden'>
-                      <img
-                          src="one-piece.jpg"
-                          alt='Movie image'
-                          className='transition-transform duration-300 ease-in-out group-hover:scale-125 w-[243px] h-[136px]'
-                      />
-                      <div className='absolute top-0 left-2 text-red-600 font-bold'>N</div>
-                  </div>
-                  <p className='mt-2 text-left text-lg truncate'>One Piece</p>
-              </Link>
-              <Link to={`/watch/1`} className=' max-w-[243px] relative group' >
-                  <div className='rounded-lg overflow-hidden'>
-                      <img
-                          src="one-piece.jpg"
-                          alt='Movie image'
-                          className='transition-transform duration-300 ease-in-out group-hover:scale-125 w-[243px] h-[136px]'
-                      />
-                      <div className='absolute top-0 left-2 text-red-600 font-bold'>N</div>
-                  </div>
-                  <p className='mt-2 text-left text-lg truncate'>One Piece</p>
-              </Link>
-              <Link to={`/watch/1`} className=' max-w-[243px] relative group' >
-                  <div className='rounded-lg overflow-hidden'>
-                      <img
-                          src="one-piece.jpg"
-                          alt='Movie image'
-                          className='transition-transform duration-300 ease-in-out group-hover:scale-125 w-[243px] h-[136px]'
-                      />
-                      <div className='absolute top-0 left-2 text-red-600 font-bold'>N</div>
-                  </div>
-                  <p className='mt-2 text-left text-lg truncate'>One Piece</p>
-              </Link>
-              <Link to={`/watch/1`} className=' max-w-[243px] relative group' >
-                  <div className='rounded-lg overflow-hidden'>
-                      <img
-                          src="one-piece.jpg"
-                          alt='Movie image'
-                          className='transition-transform duration-300 ease-in-out group-hover:scale-125 w-[243px] h-[136px]'
-                      />
-                      <div className='absolute top-0 left-2 text-red-600 font-bold'>N</div>
-                  </div>
-                  <p className='mt-2 text-left text-lg truncate'>One Piece</p>
-              </Link>
-              <Link to={`/watch/1`} className=' max-w-[243px] relative group' >
-                  <div className='rounded-lg overflow-hidden'>
-                      <img
-                          src="one-piece.jpg"
-                          alt='Movie image'
-                          className='transition-transform duration-300 ease-in-out group-hover:scale-125 w-[243px] h-[136px]'
-                      />
-                      <div className='absolute top-0 left-2 text-red-600 font-bold'>N</div>
-                  </div>
-                  <p className='mt-2 text-left text-lg truncate'>One Piece</p>
-              </Link>
-              <Link to={`/watch/1`} className=' max-w-[243px] relative group' >
-                  <div className='rounded-lg overflow-hidden'>
-                      <img
-                          src="one-piece.jpg"
-                          alt='Movie image'
-                          className='transition-transform duration-300 ease-in-out group-hover:scale-125 w-[243px] h-[136px]'
-                      />
-                      <div className='absolute top-0 left-2 text-red-600 font-bold'>N</div>
-                  </div>
-                  <p className='mt-2 text-left text-lg truncate'>One Piece</p>
-              </Link>
-              <Link to={`/watch/1`} className=' max-w-[243px] relative group' >
-                  <div className='rounded-lg overflow-hidden'>
-                      <img
-                          src="one-piece.jpg"
-                          alt='Movie image'
-                          className='transition-transform duration-300 ease-in-out group-hover:scale-125 w-[243px] h-[136px]'
-                      />
-                      <div className='absolute top-0 left-2 text-red-600 font-bold'>N</div>
-                  </div>
-                  <p className='mt-2 text-left text-lg truncate'>One Piece</p>
-              </Link>
-              <Link to={`/watch/1`} className=' max-w-[243px] relative group' >
-                  <div className='rounded-lg overflow-hidden'>
-                      <img
-                          src="one-piece.jpg"
-                          alt='Movie image'
-                          className='transition-transform duration-300 ease-in-out group-hover:scale-125 w-[243px] h-[136px]'
-                      />
-                      <div className='absolute top-0 left-2 text-red-600 font-bold'>N</div>
-                  </div>
-                  <p className='mt-2 text-left text-lg truncate'>One Piece</p>
-              </Link>
-              <Link to={`/watch/1`} className=' max-w-[243px] relative group' >
-                  <div className='rounded-lg overflow-hidden'>
-                      <img
-                          src="one-piece.jpg"
-                          alt='Movie image'
-                          className='transition-transform duration-300 ease-in-out group-hover:scale-125 w-[243px] h-[136px]'
-                      />
-                      <div className='absolute top-0 left-2 text-red-600 font-bold'>N</div>
-                  </div>
-                  <p className='mt-2 text-left text-lg truncate'>One Piece</p>
-              </Link>
-              <Link to={`/watch/1`} className=' max-w-[243px] relative group' >
-                  <div className='rounded-lg overflow-hidden'>
-                      <img
-                          src="one-piece.jpg"
-                          alt='Movie image'
-                          className='transition-transform duration-300 ease-in-out group-hover:scale-125 w-[243px] h-[136px]'
-                      />
-                      <div className='absolute top-0 left-2 text-red-600 font-bold'>N</div>
-                  </div>
-                  <p className='mt-2 text-left text-lg truncate'>One Piece</p>
-              </Link>
+    const ServiceData = [
+      {
+        id: "1",
+        content: "One piece",
+        backgroundImage: "one-piece.jpg",
+      },
+      {
+        id: "2",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        backgroundImage: "phi-vu-trieu-do.webp",
+      },
+      {
+        id: "3",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        backgroundImage: "one-piece.jpg",
+      },
+      {
+        id: "4",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        backgroundImage: "one-piece.jpg",
+      },
+      {
+        id: "5",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 123",
+        backgroundImage: "one-piece.jpg",
+      },
+      {
+        id: "6",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        backgroundImage: "one-piece.jpg",
+      },
+      {
+        id: "7",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        backgroundImage: "one-piece.jpg",
+      },
+      {
+        id: "8",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        backgroundImage: "one-piece.jpg",
+      },
+      {
+        id: "9",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        backgroundImage: "one-piece.jpg",
+      },
+      {
+        id: "10",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 123",
+        backgroundImage: "one-piece.jpg",
+      },
+      {
+        id: "11",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        backgroundImage: "one-piece.jpg",
+      },
+      {
+        id: "12",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        backgroundImage: "one-piece.jpg",
+      },
+    ];
+  
+    return (
+        <>
+        <div className="bg-black px-6 py-4">
+        <h3 className="text-white font-bold text-2xl mb-4">Top 10 bộ phim hay Việt Nam</h3>
+        <div className="flex flex-col gap-2 h-[auto]">
+          <Swiper
+            modules={[FreeMode, Pagination]}
+            spaceBetween={10}
+            slidesPerView={6} // Set default slidesPerView
+            freeMode={true}
+         
+            className="max-w[100%]  !mx-0"
+            breakpoints={{
+              640: {
+                slidesPerView: 1,
+              
+              },
+              768: {
+                slidesPerView: 5, // Slide 3 at 768px width
+               
+              },
+              1024: {
+                slidesPerView: 5, // Slide 4 at 1024px width
+               
+              },
+              2048: {
+                slidesPerView: 5, // Slide 5 at 2048px width
+               
+                }
+            }}
+          >
+            {ServiceData.map((service) => (
+              <SwiperSlide key={service.id}>
+                <div className="flex flex-col gap-2 w-auto h-auto">
+                  <img
+                    src={service.backgroundImage}
+                    className="w-full h-[177px] object-cover rounded-lg"
+                    alt="One Piece"
+                  />
+                  <span className="text-white text-xl truncate ">{service.content}</span>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
       </div>
-      <button className=' absolute top-[45px] left-0 bottom-[45px] text-[100px] flex justify-center cursor-pointer'>&#60;</button>
-       <button className=''>&#62;</button>
-  </div>
-  )
-}
+      <div className="bg-black px-6 py-4">
+        <h3 className="text-white font-bold text-2xl mb-4">Top 10 bộ phim hay Việt Nam</h3>
+        <div className="flex flex-col gap-2 h-[auto]">
+          <Swiper
+            modules={[FreeMode, Pagination]}
+            spaceBetween={10}
+            slidesPerView={6} // Set default slidesPerView
+            freeMode={true}
+            pagination={{ clickable: true }}
+            className="max-w[100%]  !mx-0"
+            breakpoints={{
+              640: {
+                slidesPerView: 1,
+              
+              },
+              768: {
+                slidesPerView: 5, // Slide 3 at 768px width
+               
+              },
+              1024: {
+                slidesPerView: 5, // Slide 4 at 1024px width
+               
+              },
+              2048: {
+                slidesPerView: 5, // Slide 5 at 2048px width
+               
+                }
+            }}
+          >
+            {ServiceData.map((service) => (
+              <SwiperSlide key={service.id}>
+                <div className="flex flex-col gap-2 w-auto h-auto">
+                  <img
+                    src={service.backgroundImage}
+                    className="w-full h-[177px] object-cover rounded-lg"
+                    alt="One Piece"
+                  />
+                  <span className="text-white text-xl truncate">{service.content}</span>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </div>
+        </>
+      
+
+      
+    );
+  };
 
 export default MovieSlider
